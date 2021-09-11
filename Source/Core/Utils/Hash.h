51a42e8282f3a9fd904e0e276445d50def54d389
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-09-11 17:34:19 +0800

    add hash combine

---
+++ Source/Core/Utils/Hash.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Core/Engine/PCH.h"
+
+namespace Ilum
+{
+template <typename T>
+constexpr void hash_combine(uint32_t &seed, const T &t)
+{
+	std::hash<T> hasher;
+	seed ^= hasher(t) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+}
