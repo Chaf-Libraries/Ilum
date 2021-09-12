5fc4b3c6cb81ad99f5c525230401df9a07de996f
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-09-12 22:33:04 +0800

    move vulkan logger

---
+++ Source/Core/Vulkan/Utils/Logger.cpp
@@ -0,0 +1,20 @@
+#include "Logger.hpp"
+
+namespace Ilum::Vulkan
+{
+const bool check(VkResult result)
+{
+	if (result == VK_SUCCESS)
+	{
+		return true;
+	}
+
+	VK_ERROR("{}", std::to_string(result));
+	return false;
+}
+
+void _assert(VkResult result)
+{
+	assert(result == VK_SUCCESS);
+}
+}
