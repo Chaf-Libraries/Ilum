b62bba94e0b5972bbb6d2c5fcb84b673b75fb4c0
chaphlagical <418199378@qq.com>
2021-09-13 21:11:31 +0800

    add Renderer

---
+++ Source/Core/Renderer/Renderer.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Core/Engine/PCH.hpp"
+#include "Core/Engine/Subsystem.hpp"
+
+
+
+namespace Ilum
+{
+class Renderer : public TSubsystem<Renderer>
+{
+  public:
+  private:
+
+};
+}        // namespace Ilum
