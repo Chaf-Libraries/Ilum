96faf7c64a238b13e0be46800c05a1f7e7d8a132
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-09-14 19:40:58 +0800

    update

--- Source/Core/Engine/Vulkan/Vulkan.hpp
+++ Source/Core/Engine/Vulkan/Vulkan.hpp
@@ -3,11 +3,19 @@
 #include <volk.h>
 #include <vk_mem_alloc.h>
 
+#include <string>
+
 #define VK_CHECK(result) Ilum::vk_check(result)
 #define VK_ASSERT(result) Ilum::vk_assert(result)
 
 namespace Ilum
 {
 const bool vk_check(VkResult result);
+
 void vk_assert(VkResult result);
+}
+
+namespace std
+{
+std::string to_string(VkResult result);
 }
