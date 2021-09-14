299b5a3f0d4d623bc5b3f6f8200223d23d835f2b
chaphlagical <418199378@qq.com>
2021-09-14 09:08:11 +0800

    add const

--- Source/Core/Device/Instance.hpp
+++ Source/Core/Device/Instance.hpp
@@ -32,9 +32,9 @@ class Instance
 	VkInstance m_handle = VK_NULL_HANDLE;
 
 #ifdef _DEBUG
-	std::vector<const char *>                 m_extensions            = {"VK_KHR_surface", "VK_KHR_win32_surface", "VK_EXT_debug_report", "VK_EXT_debug_utils"};
-	std::vector<const char *>                 m_validation_layers     = {"VK_LAYER_KHRONOS_validation"};
-	std::vector<VkValidationFeatureEnableEXT> m_validation_extensions = {
+	const std::vector<const char *>                 m_extensions            = {"VK_KHR_surface", "VK_KHR_win32_surface", "VK_EXT_debug_report", "VK_EXT_debug_utils"};
+	const std::vector<const char *>           m_validation_layers     = {"VK_LAYER_KHRONOS_validation"};
+	const std::vector<VkValidationFeatureEnableEXT> m_validation_extensions = {
 	    VK_VALIDATION_FEATURE_ENABLE_GPU_ASSISTED_RESERVE_BINDING_SLOT_EXT,
 	    VK_VALIDATION_FEATURE_ENABLE_GPU_ASSISTED_EXT,
 	    VK_VALIDATION_FEATURE_ENABLE_BEST_PRACTICES_EXT,
