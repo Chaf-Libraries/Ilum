9ee00979e984ee5ecec29e6ab9035af586e24ad4
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-09-15 13:13:13 +0800

    update

--- Source/Core/Device/LogicalDevice.cpp
+++ Source/Core/Device/LogicalDevice.cpp
@@ -1,7 +1,340 @@
 #include "LogicalDevice.hpp"
+#include "Instance.hpp"
+#include "PhysicalDevice.hpp"
+#include "Surface.hpp"
 
 namespace Ilum
 {
-const std::vector<const char *> LogicalDevice::extensions = {};
+const std::vector<const char *> LogicalDevice::extensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+    VK_KHR_ACCELERATION_STRUCTURE_EXTENSION_NAME,
+    VK_KHR_RAY_TRACING_PIPELINE_EXTENSION_NAME,
+    VK_KHR_DEFERRED_HOST_OPERATIONS_EXTENSION_NAME};
 
+inline const std::vector<const char *> get_device_extension_support(const PhysicalDevice &physical_device, const std::vector<const char *> &extensions)
+{
+	std::vector<const char *> result;
+
+	uint32_t device_extension_properties_count = 0;
+	vkEnumerateDeviceExtensionProperties(physical_device, nullptr, &device_extension_properties_count, nullptr);
+
+	std::vector<VkExtensionProperties> extension_properties(device_extension_properties_count);
+	vkEnumerateDeviceExtensionProperties(physical_device, nullptr, &device_extension_properties_count, extension_properties.data());
+
+	for (auto &device_extension : LogicalDevice::extensions)
+	{
+		bool enable = false;
+		for (auto &support_extension : extension_properties)
+		{
+			if (std::strcmp(device_extension, support_extension.extensionName) == 0)
+			{
+				result.push_back(device_extension);
+				enable = true;
+				VK_INFO("Enable device extension: {}", device_extension);
+				break;
+			}
+		}
+
+		if (!enable)
+		{
+			VK_WARN("Device extension {} is not supported", device_extension);
+		}
+	}
+
+	return result;
 }
+
+inline std::optional<uint32_t> get_queue_family_index(const std::vector<VkQueueFamilyProperties> &queue_family_properties, VkQueueFlagBits queue_flag)
+{
+	// Dedicated queue for compute
+	// Try to find a queue family index that supports compute but not graphics
+	if (queue_flag & VK_QUEUE_COMPUTE_BIT)
+	{
+		for (uint32_t i = 0; i < static_cast<uint32_t>(queue_family_properties.size()); i++)
+		{
+			if ((queue_family_properties[i].queueFlags & queue_flag) && ((queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) == 0))
+			{
+				return i;
+				break;
+			}
+		}
+	}
+
+	// Dedicated queue for transfer
+	// Try to find a queue family index that supports transfer but not graphics and compute
+	if (queue_flag & VK_QUEUE_TRANSFER_BIT)
+	{
+		for (uint32_t i = 0; i < static_cast<uint32_t>(queue_family_properties.size()); i++)
+		{
+			if ((queue_family_properties[i].queueFlags & queue_flag) && ((queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) == 0) && ((queue_family_properties[i].queueFlags & VK_QUEUE_COMPUTE_BIT) == 0))
+			{
+				return i;
+				break;
+			}
+		}
+	}
+
+	// For other queue types or if no separate compute queue is present, return the first one to support the requested flags
+	for (uint32_t i = 0; i < static_cast<uint32_t>(queue_family_properties.size()); i++)
+	{
+		if (queue_family_properties[i].queueFlags & queue_flag)
+		{
+			return i;
+			break;
+		}
+	}
+
+	return std::optional<uint32_t>();
+}
+
+LogicalDevice::LogicalDevice(const Instance &instance, const PhysicalDevice &physical_device, const Surface &surface) :
+    m_instance(instance),
+    m_physical_device(physical_device),
+    m_surface(surface)
+{
+	// Queue supporting
+	uint32_t queue_family_property_count = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(m_physical_device, &queue_family_property_count, nullptr);
+	std::vector<VkQueueFamilyProperties> queue_family_properties(queue_family_property_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(m_physical_device, &queue_family_property_count, queue_family_properties.data());
+
+	std::optional<uint32_t> graphics_family, compute_family, transfer_family, present_family;
+
+	graphics_family = get_queue_family_index(queue_family_properties, VK_QUEUE_GRAPHICS_BIT);
+	transfer_family = get_queue_family_index(queue_family_properties, VK_QUEUE_TRANSFER_BIT);
+	compute_family  = get_queue_family_index(queue_family_properties, VK_QUEUE_COMPUTE_BIT);
+
+	for (uint32_t i = 0; i < queue_family_property_count; i++)
+	{
+		// Check for presentation support
+		VkBool32 present_support;
+		vkGetPhysicalDeviceSurfaceSupportKHR(m_physical_device, i, m_surface, &present_support);
+
+		if (queue_family_properties[i].queueCount > 0 && present_support)
+		{
+			present_family   = i;
+			m_present_family = i;
+			m_present_queues.resize(queue_family_properties[i].queueCount);
+			break;
+		}
+	}
+
+	if (graphics_family.has_value())
+	{
+		m_graphics_family = graphics_family.value();
+		m_support_queues |= VK_QUEUE_GRAPHICS_BIT;
+		m_graphics_queues.resize(queue_family_properties[m_graphics_family].queueCount);
+	}
+
+	if (compute_family.has_value())
+	{
+		m_compute_family = compute_family.value();
+		m_support_queues |= VK_QUEUE_COMPUTE_BIT;
+		m_compute_queues.resize(queue_family_properties[m_compute_family].queueCount);
+	}
+
+	if (transfer_family.has_value())
+	{
+		m_transfer_family = transfer_family.value();
+		m_support_queues |= VK_QUEUE_TRANSFER_BIT;
+		m_transfer_queues.resize(queue_family_properties[m_transfer_family].queueCount);
+	}
+
+	if (!graphics_family)
+	{
+		throw std::runtime_error("Failed to find queue graphics family support!");
+	}
+
+	// Create device queue
+	std::vector<VkDeviceQueueCreateInfo> queue_create_infos;
+
+	uint32_t max_count = 0;
+	for (auto &queue_family_property : queue_family_properties)
+	{
+		max_count = max_count < queue_family_property.queueCount ? queue_family_property.queueCount : max_count;
+	}
+
+	std::vector<float> queue_priorities(max_count, 1.f);
+
+	if (m_support_queues & VK_QUEUE_GRAPHICS_BIT)
+	{
+		VkDeviceQueueCreateInfo graphics_queue_create_info = {};
+		graphics_queue_create_info.sType                   = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		graphics_queue_create_info.queueFamilyIndex        = m_graphics_family;
+		graphics_queue_create_info.queueCount              = queue_family_properties[m_graphics_family].queueCount;
+		graphics_queue_create_info.pQueuePriorities        = queue_priorities.data();
+		queue_create_infos.emplace_back(graphics_queue_create_info);
+	}
+	else
+	{
+		m_graphics_family = 0;
+	}
+
+	if (m_support_queues & VK_QUEUE_COMPUTE_BIT && m_compute_family != m_graphics_family)
+	{
+		VkDeviceQueueCreateInfo compute_queue_create_info = {};
+		compute_queue_create_info.sType                   = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		compute_queue_create_info.queueFamilyIndex        = m_compute_family;
+		compute_queue_create_info.queueCount              = queue_family_properties[m_compute_family].queueCount;
+		compute_queue_create_info.pQueuePriorities        = queue_priorities.data();
+		queue_create_infos.emplace_back(compute_queue_create_info);
+	}
+	else
+	{
+		m_compute_family = m_graphics_family;
+	}
+
+	if (m_support_queues & VK_QUEUE_TRANSFER_BIT && m_transfer_family != m_graphics_family && m_transfer_family != m_compute_family)
+	{
+		VkDeviceQueueCreateInfo transfer_queue_create_info = {};
+		transfer_queue_create_info.sType                   = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		transfer_queue_create_info.queueFamilyIndex        = m_transfer_family;
+		transfer_queue_create_info.queueCount              = queue_family_properties[m_transfer_family].queueCount;
+		transfer_queue_create_info.pQueuePriorities        = queue_priorities.data();
+		queue_create_infos.emplace_back(transfer_queue_create_info);
+	}
+	else
+	{
+		m_transfer_family = m_graphics_family;
+	}
+
+	// Enable logical device features
+	auto &                   physical_device_features = m_physical_device.getFeatures();
+
+#define ENABLE_DEVICE_FEATURE(feature)                             \
+	if (physical_device_features.feature)                          \
+	{                                                              \
+		m_enabled_features.feature = VK_TRUE;                        \
+		VK_INFO("Physical device feature enable: {}", #feature)    \
+	}                                                              \
+	else                                                           \
+	{                                                              \
+		VK_WARN("Physical device feature not found: {}", #feature) \
+	}
+
+	ENABLE_DEVICE_FEATURE(sampleRateShading);
+	ENABLE_DEVICE_FEATURE(fillModeNonSolid);
+	ENABLE_DEVICE_FEATURE(wideLines);
+	ENABLE_DEVICE_FEATURE(samplerAnisotropy);
+	ENABLE_DEVICE_FEATURE(textureCompressionBC);
+	ENABLE_DEVICE_FEATURE(textureCompressionASTC_LDR);
+	ENABLE_DEVICE_FEATURE(textureCompressionETC2);
+	ENABLE_DEVICE_FEATURE(vertexPipelineStoresAndAtomics);
+	ENABLE_DEVICE_FEATURE(fragmentStoresAndAtomics);
+	ENABLE_DEVICE_FEATURE(shaderStorageImageExtendedFormats);
+	ENABLE_DEVICE_FEATURE(shaderStorageImageWriteWithoutFormat);
+	ENABLE_DEVICE_FEATURE(geometryShader);
+	ENABLE_DEVICE_FEATURE(tessellationShader);
+	ENABLE_DEVICE_FEATURE(multiViewport);
+	ENABLE_DEVICE_FEATURE(imageCubeArray);
+
+	// Get support extensions
+	auto support_extensions = get_device_extension_support(m_physical_device, extensions);
+
+	// Create device
+	VkDeviceCreateInfo device_create_info   = {};
+	device_create_info.sType                = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_create_info.queueCreateInfoCount = static_cast<uint32_t>(queue_create_infos.size());
+	device_create_info.pQueueCreateInfos    = queue_create_infos.data();
+	if (!Instance::validation_layers.empty())
+	{
+		device_create_info.enabledLayerCount   = static_cast<uint32_t>(Instance::validation_layers.size());
+		device_create_info.ppEnabledLayerNames = Instance::validation_layers.data();
+	}
+	device_create_info.enabledExtensionCount   = static_cast<uint32_t>(support_extensions.size());
+	device_create_info.ppEnabledExtensionNames = support_extensions.data();
+	device_create_info.pEnabledFeatures        = &m_enabled_features;
+
+	if (!VK_CHECK(vkCreateDevice(m_physical_device, &device_create_info, nullptr, &m_handle)))
+	{
+		return;
+	}
+
+	// Volk load context
+	volkLoadDevice(m_handle);
+
+	// Get device queues
+	for (uint32_t i = 0; i < static_cast<uint32_t>(m_graphics_queues.size()); i++)
+	{
+		vkGetDeviceQueue(m_handle, m_graphics_family, i, &m_graphics_queues[i]);
+	}
+
+	for (uint32_t i = 0; i < static_cast<uint32_t>(m_compute_queues.size()); i++)
+	{
+		vkGetDeviceQueue(m_handle, m_compute_family, i, &m_compute_queues[i]);
+	}
+
+	for (uint32_t i = 0; i < static_cast<uint32_t>(m_transfer_queues.size()); i++)
+	{
+		vkGetDeviceQueue(m_handle, m_transfer_family, i, &m_transfer_queues[i]);
+	}
+
+	for (uint32_t i = 0; i < static_cast<uint32_t>(m_present_queues.size()); i++)
+	{
+		vkGetDeviceQueue(m_handle, m_present_family, i, &m_present_queues[i]);
+	}
+}
+
+LogicalDevice::~LogicalDevice()
+{
+	if (VK_CHECK(vkDeviceWaitIdle(m_handle)))
+	{
+		vkDestroyDevice(m_handle, nullptr);
+	}
+}
+
+LogicalDevice::operator const VkDevice &() const
+{
+	return m_handle;
+}
+
+const VkDevice &LogicalDevice::getLogicalDevice() const
+{
+	return m_handle;
+}
+
+const VkPhysicalDeviceFeatures &LogicalDevice::getEnabledFeatures() const
+{
+	return m_enabled_features;
+}
+
+const uint32_t LogicalDevice::getGraphicsFamily() const
+{
+	return m_graphics_family;
+}
+
+const uint32_t LogicalDevice::getComputeFamily() const
+{
+	return m_compute_family;
+}
+
+const uint32_t LogicalDevice::getTransferFamily() const
+{
+	return m_transfer_family;
+}
+
+const uint32_t LogicalDevice::getPresentFamily() const
+{
+	return m_present_family;
+}
+
+const std::vector<VkQueue> &LogicalDevice::getGraphicsQueues() const
+{
+	return m_graphics_queues;
+}
+
+const std::vector<VkQueue> &LogicalDevice::getComputeQueues() const
+{
+	return m_compute_queues;
+}
+
+const std::vector<VkQueue> &LogicalDevice::getTransferQueues() const
+{
+	return m_transfer_queues;
+}
+
+const std::vector<VkQueue> &LogicalDevice::getPresentQueues() const
+{
+	return m_present_queues;
+}
+}        // namespace Ilum
--- Source/Core/Device/LogicalDevice.hpp
+++ Source/Core/Device/LogicalDevice.hpp
@@ -4,13 +4,60 @@
 
 namespace Ilum
 {
+class Instance;
+class PhysicalDevice;
+class Surface;
+
 class LogicalDevice
 {
   public:
+	LogicalDevice(const Instance &instance, const PhysicalDevice &physical_device, const Surface &surface);
+
+	~LogicalDevice();
+
+	operator const VkDevice &() const;
+
+	const VkDevice &getLogicalDevice() const;
+
+	const VkPhysicalDeviceFeatures &getEnabledFeatures() const;
+
+	const uint32_t getGraphicsFamily() const;
+
+	const uint32_t getComputeFamily() const;
+
+	const uint32_t getTransferFamily() const;
+
+	const uint32_t getPresentFamily() const;
+
+	const std::vector<VkQueue>& getGraphicsQueues() const;
+
+	const std::vector<VkQueue>& getComputeQueues() const;
+
+	const std::vector<VkQueue>& getTransferQueues() const;
+
+	const std::vector<VkQueue>& getPresentQueues() const;
+
   public:
 	// Extensions
 	static const std::vector<const char *> extensions;
 
   private:
+	const Instance &      m_instance;
+	const PhysicalDevice &m_physical_device;
+	const Surface &       m_surface;
+
+	VkDevice                 m_handle           = VK_NULL_HANDLE;
+	VkPhysicalDeviceFeatures m_enabled_features = {};
+
+	VkQueueFlags m_support_queues  = {};
+	uint32_t     m_graphics_family = 0;
+	uint32_t     m_compute_family  = 0;
+	uint32_t     m_transfer_family = 0;
+	uint32_t     m_present_family  = 0;
+
+	std::vector<VkQueue> m_graphics_queues;
+	std::vector<VkQueue> m_present_queues;
+	std::vector<VkQueue> m_compute_queues;
+	std::vector<VkQueue> m_transfer_queues;
 };
 }        // namespace Ilum
