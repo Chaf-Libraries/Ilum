8a0aa827ba6458324969fbb6da1c541d12d04cdc
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-10-02 18:15:32 +0800

    update

---
+++ Source/Core/Resource/IResource.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <typeindex>
+
+namespace Ilum
+{
+	template<typename T>
+	class IResource
+	{
+	  public:
+	    IResource() = default;
+
+		~IResource() = default;
+
+		std::type_index getType() const
+		{
+		    return typeid(T);
+		}
+	};
+}
