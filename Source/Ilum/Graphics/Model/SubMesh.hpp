e0e1dce57de553965190fbf886093ed2b2dce1a5
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-10-25 22:47:36 +0800

    add model

---
+++ Source/Ilum/Graphics/Model/SubMesh.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Geometry/Bound/Bound.hpp"
+#include "Geometry/Mesh/TriMesh.hpp"
+#include "Vertex.hpp"
+
+namespace Ilum
+{
+struct SubMesh
+{
+  public:
+	SubMesh(std::vector<Vertex> &&vertices, std::vector<uint32_t> &&indices, uint32_t index_offset);
+
+	~SubMesh() = default;
+
+	SubMesh(const SubMesh &) = delete;
+
+	SubMesh &operator=(const SubMesh &) = delete;
+
+	SubMesh(SubMesh &&other) noexcept;
+
+	SubMesh &operator=(SubMesh &&other) noexcept;
+
+	uint32_t getVertexCount() const;
+
+	uint32_t getIndexCount() const;
+
+	uint32_t getIndexOffset() const;
+
+	const std::vector<Vertex> &getVertices() const;
+
+	const std::vector<uint32_t> &getIndices() const;
+
+  private:
+	uint32_t m_index_offset   = 0;
+
+	std::vector<Vertex>   m_vertices;
+	std::vector<uint32_t> m_indices;
+
+	scope<Bound> m_bound;
+};
+}        // namespace Ilum
