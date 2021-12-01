66f105d550e7890fcf67d42fab42e0716d031ebe
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-12-01 13:04:25 +0800

    add linux support

--- Source/Ilum/Utils/Platform.hpp
+++ Source/Ilum/Utils/Platform.hpp
@@ -4,8 +4,10 @@
 #	ifdef _WIN64
 #		define ILUM_PLATFORM_WINDOWS
 #	else
-#		error "x86 builds are not supported!"
+#		warn "x86 builds are not supported!"
 #	endif        // _WIN64
+#elif __linux__
+#	define ILUM_PLATFORM_LINUX
 #else
 #	error "Unknow platform!"
 #endif        // _WIN32
