de3c149b6f9b2bbb6adceec371c5096478c928cc
Wenbo Chen <chaf@mail.ustc.edu.cn>
2022-07-05 23:12:35 +0800

    sol2+lua test

--- Source/Ilum/main.cpp
+++ Source/Ilum/main.cpp
@@ -2,8 +2,16 @@
 
 #include <Core/Path.hpp>
 
+#include <sol/sol.hpp>
+
 int main()
 {
+	sol::state lua;
+	int        x = 0;
+	lua.set_function("beep", [&x] { ++x; std::cout<<"Test"<<std::endl; });
+	lua.script("beep()");
+	assert(x == 1);
+
 	Ilum::Path::GetInstance().SetCurrent(std::string(PROJECT_SOURCE_DIR));
 
 	{
