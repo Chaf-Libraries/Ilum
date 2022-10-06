aa2b9071a9cd4a5a34b67fdfe9e67c66bc87237f
Wenbo Chen <chaf@mail.ustc.edu.cn>
2022-10-06 17:26:51 +0800

    bugfix

--- Source/Editor/Widget/SceneView.cpp
+++ Source/Editor/Widget/SceneView.cpp
@@ -73,9 +73,8 @@ void SceneView::DisplayPresent()
 
 	if (ImGui::BeginDragDropTarget())
 	{
-		if (const auto *pay_load = ImGui::AcceptDragDropPayload(typeid(ResourceType::Scene).name())
+		if (const auto *pay_load = ImGui::AcceptDragDropPayload(typeid(ResourceType::Scene).name()))
 		{
-			ASSERT(pay_load->DataSize == sizeof(size_t));
 			size_t uuid = *static_cast<size_t *>(pay_load->Data);
 
 			auto *resource  = p_editor->GetRenderer()->GetResourceManager()->GetResource<ResourceType::Scene>(uuid);
