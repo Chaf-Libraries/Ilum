9fceb77f51b8f2ea0d63451c86068270cd978c29
Wenbo Chen <35370033+Chaphlagical@users.noreply.github.com>
2023-03-23 17:57:14 +0800

    imgui bugfix

--- Source/External/imgui_tools/implot/implot.h
+++ Source/External/imgui_tools/implot/implot.h
@@ -45,6 +45,9 @@
 // [SECTION] Obsolete API
 
 #pragma once
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
+#	define IMGUI_DEFINE_MATH_OPERATORS
+#endif
 #include "imgui.h"
 
 //-----------------------------------------------------------------------------
